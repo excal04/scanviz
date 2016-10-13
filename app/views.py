286e7722d81ac4b74475bed4bf8eb92cd802d44b
excal04 <excalibrus04@gmail.com>
2016-10-13 10:16:43 +0800

    scans stored to elasticsearch

--- app/views.py
+++ app/views.py
@@ -8,20 +8,27 @@ from werkzeug.utils import secure_filename
 
 @app.route('/')
 def index():
-    res = es.get(index="dfir", doc_type="scan", id=2)['_source']
-    return render_template('index.html', title="Home", res=res)
+    return render_template('index.html', title="Home")
 
 
 @app.route('/upload', methods=('GET', 'POST'))
 def upload():
     form = ScanForm()
     if form.validate_on_submit():
+        # we want either text upload of file upload
+        contents = None
         if form.fileup.data:
-            print form.fileup.data.read()
-            flash('scan file uploaded!')
-            return redirect(url_for('index'))
+            contents = form.fileup.data.read()
         elif form.textup.data:
-            print form.textup.data
-            flash('scan text uploaded!')
+            contents = form.textup.data
+
+        if contents:
+            res = es.index(index=ELASTIC_SCAN_INDEX,
+                doc_type=ELASTIC_SCAN_DOC_TYPE,
+                body=json.loads(contents))
+            if res:
+                flash('scan uploaded!')
+            else:
+                flash('error indexing data')
             return redirect(url_for('index'))
     return render_template('upload_scan.html', form=form)
